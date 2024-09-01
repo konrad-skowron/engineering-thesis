22edfca51269050d8d6836ed1fb8035d18098e2b
Konrad Skowron <73618275+Konrad-Skowron@users.noreply.github.com>
2024-09-01 23:38:15 +0200

    initial commit

---
+++ src/app/account/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+export default function Account() {
+  const [user, setUser] = useState("Anonymous");
+
+  return (
+    <>
+      <p>Hello {user}</p>
+      <Link href="/create">
+        <button>+ Create poll</button>
+      </Link>
+    </>
+  );
+}
