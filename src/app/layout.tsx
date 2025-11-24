91d9932560cf82daaa3a4504ce726c167e1e2d77
Konrad Skowron <73618275+Konrad-Skowron@users.noreply.github.com>
2025-11-24 22:09:33 +0100

    Add microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <ColorSchemeScript defaultColorScheme="auto" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "ubael56mbm");
+            `,
+          }}
+        />
       </head>
       <body>
         <MantineProvider defaultColorScheme="auto">
